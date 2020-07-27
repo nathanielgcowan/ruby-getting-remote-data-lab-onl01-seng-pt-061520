ce01ebd71bdb3a18eb15cb545460ee1e98ac7a86
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-27 20:57:52 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -14,6 +14,11 @@ class GetRequester
     response = Net::HTTP.get_response(@uri)
     response.body
   end
+    def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
   
   def parse_json
     JSON.parse(get_response_body)
