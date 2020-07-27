b094ee779def411509622fda4af6fd45a381efab
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-27 20:43:09 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,6 +1,10 @@
 # Write your code here
 class GetRequester
   
+  def initialize(url)
+    @url = url
+  end
+  
   def get_response_body
     uri = URI.parse(@url)
     response = NET::HTTP.get_response(uri)
